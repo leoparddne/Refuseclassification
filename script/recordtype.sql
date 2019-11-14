c5350d3d9b7178a956c26250eeddfd443a352d34
ives <1076010937@qq.com>
2019-11-14 13:11:17 +0800

    Add files via upload

---
+++ script/recordtype.sql
@@ -0,0 +1,5 @@
+INSERT INTO `recordtype` VALUES (1, '干垃圾', 1);
+INSERT INTO `recordtype` VALUES (2, '湿垃圾', 1);
+INSERT INTO `recordtype` VALUES (3, '有害垃圾', 1);
+INSERT INTO `recordtype` VALUES (4, '可回收物', 1);
+INSERT INTO `recordtype` VALUES (5, '大件垃圾', 1);
